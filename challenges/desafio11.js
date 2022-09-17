9c8412a4f28dbbc2da1aa1586d759b7e4cf6e375
IgorHamzi <igorwhamzi@gmail.com>
2022-09-17 19:05:19 -0300

    feat: add challenge 11

---
+++ challenges/desafio11.js
@@ -0,0 +1,12 @@
+// Retorne o nome do produto, a quantidade de curtidas e quantos itens foram vendidos dos produtos que não sejam iguais a Big Mac e McChicken
+
+db.produtos.find({
+  $nor: [
+    { nome: "McChicken" },
+    { nome: "Big Mac" },
+  ],
+}, { _id: 0,
+    nome: 1,
+    curtidas: 1,
+    vendidos: 1,
+  });
