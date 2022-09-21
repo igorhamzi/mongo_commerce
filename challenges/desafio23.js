3b3ab7cd4d405c67cc369c1caae0feb81d004338
IgorHamzi <igorwhamzi@gmail.com>
2022-09-20 21:28:25 -0300

    feat: add challenge 23

---
+++ challenges/desafio23.js
@@ -0,0 +1,18 @@
+// Insira os valores combo e tasty no array tags de todos os sanduíches e aproveite para deixar os valores em ordem alfabética ascendente (A a Z)
+// Crie uma query que faça tanto a inserção dos valores combo e tasty no array tags de todos os sanduíches. Ordene os valores de tags em ordem alfabética ascendente.
+db.produtos.updateMany(
+  {},
+  { $push:
+    { tags:
+      { $each: ["combo", "tasty"], $sort: 1 },
+    },
+  },
+);
+// Crie uma query que retorne o nome e tags de todos os documentos.
+db.produtos.find(
+  {},
+  { nome: 1,
+    tags: 1,
+    _id: 0,
+  },
+);
